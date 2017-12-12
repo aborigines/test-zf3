6bff02bd9e08bbc2f275c243fe27defc6195827c
Watcharapol Tadtiang <watcharapol.tadtiang@seagate.com>
2017-12-12 14:12:01 +0700

    done function in Testcontroller
    Signed-off-by: Watcharapol Tadtiang <watcharapol.tadtiang@seagate.com>

--- module/Application/src/Controller/TestController.php
+++ module/Application/src/Controller/TestController.php
@@ -9,6 +9,24 @@ class TestController extends AbstractActionController
 {
     public function indexAction()
     {
+    	var_dump($this->nMultipleNMinus1(3));
+    	echo PHP_EOL;
+    	var_dump($this->findX(99));
         return new ViewModel();
     }
+
+	protected function nMultipleNMinus1($number)
+	{
+		$data = [];
+		for($n = 1; $n <= 6; $n++)
+		{
+			$data[] = $number+($n*($n-1));
+		}
+		return $data;
+	}
+
+	protected function findX($result)
+	{
+		return ($result-24)-(10*2);
+	}
 }
